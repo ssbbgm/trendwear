0acbe58bced9de1a72b5e56c4f80af6090c50afb
Sherreka <rekadarnb@gmail.com>
2022-08-19 18:57:01 -0400

    adding side info

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,7 +6,6 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
     position: relative;
 `
 
@@ -24,6 +23,42 @@ const Arrow = styled.div`
     left: ${props=> props.direction === 'left' && '10px'};
     right: ${props=> props.direction === 'right' && '10px'};
     margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+`
+
+const Wrapper = styled.div`
+    height: 100%;
+`
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+const Image = styled.img`
+    height: 70%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    
 `
 
 function Slider() {
@@ -32,6 +67,18 @@ function Slider() {
             <Arrow direction='left'>
                 <ArrowLeftOutlined />
             </Arrow>
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image src='https://cdn.pixabay.com/photo/2017/12/26/09/15/woman-3040029_1280.jpg'/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
             <Arrow direction='right'>
                 <ArrowRightOutlined />
             </Arrow>
