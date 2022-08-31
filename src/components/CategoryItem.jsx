1f2e852c62659a2d3e1966f12ffefa97c1fae577
Sherreka <rekadarnb@gmail.com>
2022-08-31 15:25:18 -0400

    adding images

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
 `
 
 const Image = styled.img`
-    
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `
 
 const Info = styled.div`
-    
+    position: absolute;
 `
 
 const Title = styled.h1`
