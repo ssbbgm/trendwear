394596f110bb8eb00996079631149b6c435b296b
Sherreka <rekadarnb@gmail.com>
2022-10-01 12:27:19 -0400

    adding footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+
+const Left = styled.div`
+
+`;
+
+const Center = styled.div`
+    
+`;
+
+const Right = styled.div`
+    
+`;
+
+
+function Footer() {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default Footer;
